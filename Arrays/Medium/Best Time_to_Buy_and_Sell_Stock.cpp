688043e49a4ea601b36fb167454a6185a4ccd3f4
Mohd Umar <44342762+maverick-010@users.noreply.github.com>
2022-05-28 01:43:26 -0700

    Create Best Time_to_Buy_and_Sell_Stock.cpp

---
+++ Arrays/Medium/Best Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    int minPrice=INT_MAX;
+        int maxPro=0;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<minPrice)minPrice=prices[i];
+            else if(prices[i]-minPrice>maxPro)maxPro=prices[i]-minPrice;
+        }
+        return maxPro;
+}
